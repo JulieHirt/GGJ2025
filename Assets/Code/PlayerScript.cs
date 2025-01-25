c8f7b418f3b3abdc5bb842ad4b4bb3be6ffac3c5
SnappyGear <nicris-gamedesign@yahoo.com>
2025-01-25 13:45:31 -0500

    FINALLY GOT PLAYER MOVEMENT AND BOUNCING

--- Assets/Code/PlayerScript.cs
+++ Assets/Code/PlayerScript.cs
@@ -1,10 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.AssetImporters;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
     public float moveSpeed;
     public float downSpeed;
+    public float sideMaxSpeed;
+    public float bounceMinimzeAmount;
+
+    //public PhysicsMaterial2D highBounceMat;
+    //public PhysicsMaterial2D lowBounceMat;
+
+    bool slammingDown = false;
+    //Vector2 lastSpeed;
 
     Rigidbody2D rb;
     bool pressedSpace = false;
@@ -24,23 +35,60 @@ public class PlayerScript : MonoBehaviour
         }
     }
 
+    void OnCollisionEnter2D()
+    {
+        /*
+        if (slammingDown)
+        {
+            Vector2 newVelocity = new Vector2(lastSpeed.x, -lastSpeed.y);
+            rb.linearVelocity = newVelocity;
+        }
+        slammingDown = false;
+        */
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) // this is so that when the ball is about to hit something at high speed, we save its speed BEFORE it hits.  If we wait till it actually hits, then velocity values get messed up for calculation
+    { 
+
+        Vector2 newVelocity = new Vector2(rb.linearVelocity.x, -(rb.linearVelocity.y / bounceMinimzeAmount));
+        rb.linearVelocity = newVelocity;
+
+        GetComponent<Collider2D>().isTrigger = false;
+    }
+
+    IEnumerator SetBounceToHigh()
+    {
+        yield return new WaitForSeconds(0.05f);
+        //rb.sharedMaterial = highBounceMat;
+    }
+
     private void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            rb.AddForce(Vector2.right * moveSpeed);
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            rb.AddForce(Vector2.left * moveSpeed);
-        }
+
+            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+            {
+                if(rb.linearVelocityX < sideMaxSpeed)
+                {
+                    rb.AddForce(Vector2.right * moveSpeed);
+                }   
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.D))
+            {
+                if (rb.linearVelocityX > sideMaxSpeed || Mathf.Abs(rb.linearVelocityX) < sideMaxSpeed)
+                {
+                    rb.AddForce(Vector2.left * moveSpeed);
+                }
+            }
+
 
         if (pressedSpace == true)
         {
-            rb.linearVelocityX = 0;
-            rb.linearVelocityY = downSpeed;
+            rb.linearVelocityY = -downSpeed;
             pressedSpace = false;
+            GetComponent<Collider2D>().isTrigger = true;
+            slammingDown = true;
         }
+
     }
 
 
