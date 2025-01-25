8744483860e86dc46a3e2b44d3710e8d4c2e89ed
BanHam600 <Mattbrown.226@yahoo.com>
2025-01-25 14:51:49 -0500

    Update DetectCollision.cs

--- Assets/Code/DetectCollision.cs
+++ Assets/Code/DetectCollision.cs
@@ -2,20 +2,47 @@ using UnityEngine;
 
 
 public class DetectCollision : MonoBehaviour
+
 {
+    private Vector3 scaleChange;
+    public GameObject Player;
+    public float PlayerScale;
+
     private void Start()
-    {
-
+    {  
+        
+        Player = GameObject.Find("Player"); 
+       scaleChange = new Vector3 (1.5f, 1.5f, 1.5f);
+    
     }
+
     void Update()
-    {
 
+    {
+       
+      
+        
 
     }
-    private void OnCollisionEnter2D (Collision2D coll)
+
+    private void OnCollisionEnter2D(Collision2D coll)
     {
-        Debug.Log ("detect");
-        Destroy(gameObject);
+        Debug.Log("detect");
+        GameObject.Find("Player");
+
+        {
+            Debug.Log("found"); 
+        }
+
+        Player.transform.localScale = PlayerScale * scaleChange;
+
+        {
+            Debug.Log(scaleChange);
+            Debug.Log(PlayerScale);
+        }
 
     }
+
+           
+
 }
